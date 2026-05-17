6c0aff961c663f835d3850dc36ff85481bb5c57a
ben-basten <45583362+ben-basten@users.noreply.github.com>
2026-05-16 20:16:49 -0400

    wip: versioned release script

--- release.sh
+++ release.sh
@@ -1,2 +1,8 @@
-GOOS=darwin GOARCH=arm64 go build -o dist/cinema-darwin-arm64 cinema.go
-GOOS=windows GOARCH=amd64 go build -o dist/cinema.exe cinema.go
+if [ -z "$1" ]; then
+  echo "Usage: ./release.sh <version>"
+  exit 1
+fi
+
+VERSION=$1
+GOOS=darwin GOARCH=arm64 go build -ldflags="-X 'main.Version=${VERSION}'" -o dist/cinema-darwin-arm64 cinema.go
+GOOS=windows GOARCH=amd64 go build -ldflags="-X 'main.Version=${VERSION}'" -o dist/cinema.exe cinema.go
